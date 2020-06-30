0a39ca02a4a960f92bf3f58b77e8975b12e78177
Luiz Saluti <luiz.saluti@gmail.com>
2020-06-30 00:00:15 -0300

    Adding POJO's that represent the model layer
    . Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    String firstName;
+    String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
