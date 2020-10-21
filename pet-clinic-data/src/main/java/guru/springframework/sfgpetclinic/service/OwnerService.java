a1ce8f11f712734f0455e3889cdcd12480db190b
Luiz Saluti <luiz.saluti@gmail.com>
2020-10-21 15:59:38 -0300

    Refactoring common methods into an interface. Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/OwnerService.java
@@ -4,9 +4,6 @@ import java.util.Set;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 	Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
